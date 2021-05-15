2c2bb220e4aef07338162403cb2d8a4bfe7e150b
Jorge Lopez <jlopezg112@miumg.edu.gt>
2021-05-14 22:01:44 -0600

    AGREGANDO ARCHIVOS

---
+++ CRUD1A/frmInicio.aspx
@@ -0,0 +1,19 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="frmInicio.aspx.cs" Inherits="CRUD1A.frmInicio" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div style="height: 113px">
+            <br />
+            <br />
+            <asp:Button ID="ButtonCargaDatos" runat="server" OnClick="ButtonCargaDatos_Click" Text="Cargar Datos A DB" />
+        </div>
+    </form>
+</body>
+</html>
---
+++ CRUD1A/frmInicio.aspx.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+using CRUD1A.CLASES.archivos;
+using CRUD1A.CLASES.baseDatos;
+
+namespace CRUD1A
+{
+    public partial class frmInicio : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+
+        private void CargarArchivoExterno()
+        {
+            string fuente = @"C:\Users\georg\source\repos\PARCIAL II\crudDB.csv";
+            clsArchivo ar = new clsArchivo();
+            clsConexion cn = new clsConexion();
+
+
+
+            //obtener todo el archivo en un arreglo
+            string[] arregloNotas = ar.LeerArchivo(fuente);
+
+            string sentencia_sql = "";
+            int NumeroLinea = 0;
+
+
+            //Iterando el arreglo, linea x linea para luego convertirlo en datos indivuales
+            foreach (string linea in arregloNotas)
+            {
+                string[] datos = linea.Split(';');
+                if (NumeroLinea>0)
+                {
+                    sentencia_sql += $"INSERT INTO tb_alumnos VALUES({datos[0]},'{datos[1]}',{datos[2]},{datos[3]},{datos[4]},{datos[5]},'{datos[6]}');\n";
+                }
+                NumeroLinea++;
+            } //end foreach
+            NumeroLinea = 0;
+            cn.EjecutaSQLDirecto(sentencia_sql); //mandar todo a la BD
+        }
+
+
+        protected void ButtonCargaDatos_Click(object sender, EventArgs e)
+        {
+            CargarArchivoExterno();
+        }
+    }
+}
